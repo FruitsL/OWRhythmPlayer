7c5ab8cfeda1ba149b07553bbd3cbef96b9ed832
FruitsL <sa4560@naver.com>
2024-09-27 21:22:05 +0900

    로그
    로그

--- src/main/java/com/fruitcoding/owrhythmplayer/util/LoggerUtil.java
+++ src/main/java/com/fruitcoding/owrhythmplayer/util/LoggerUtil.java
@@ -3,7 +3,7 @@ package com.fruitcoding.owrhythmplayer.util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class LoggerUtil {
+public class LoggerUtil { // TODO: 어떤 클래스에서 남긴 로그인지 알수있도록 개선 필요
     private static final Logger logger = LogManager.getLogger();
 
     public static void trace(Object message) {
